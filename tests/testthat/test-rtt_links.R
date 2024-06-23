181f1451ea13c075107b03028277b3121ca81ab0
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-06-23 19:33:20 +0100

    Add rtt_links_by_month tests
    2 success expectations.
    
    Correctly write testthat in Suggests.

--- tests/testthat/test-rtt_links.R
+++ tests/testthat/test-rtt_links.R
@@ -47,9 +47,37 @@ test_that("rtt_links_by_year returns the correct years", {
       "base/rtt-data-2022-23",
       "base/rtt-data-2023-24",
       "base/rtt-data-2024-25"
-    ))
+    )
+  )
 })
 
 # rtt_links_by_month ------------------------------------------------------
 
+test_that("rtt_links_by_month returns the correct month links", {
+  # Mock the HTML parsing function
+  local_mocked_bindings(
+    links_in_page = \(x) x
+  )
 
+  links_by_year <- c(
+    "not-the-right-file/Oct19",
+    "Full-CSV-data-file/Nov19",
+    "Full-CSV-data-file/Dec19",
+    "Full-CSV-data-file/Jan20",
+    "Full-CSV-data-file/Feb20",
+    "Full-CSV-data-file/Mar20",
+    "Full-CSV-data-file/Apr20",
+    "Full-CSV-data-file/May20",
+    "Full-CSV-data-file/Jun20",
+    "Full-CSV-data-file/Jul20",
+    "Full-CSV-data-file/Aug20"
+  )
+
+  # Correct file links
+  expect_equal(rtt_links_by_month(links_by_year, "2019-10", "2020-08"),
+               links_by_year[-1])
+
+  # One month only
+  expect_equal(rtt_links_by_month(links_by_year, "2020-08", "2020-08"),
+               "Full-CSV-data-file/Aug20")
+})
